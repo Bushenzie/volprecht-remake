f64ff8b516129efd2597c456279acd669ff3be27
Bushenzie <buchenzii@gmail.com>
2022-05-20 14:27:54 +0200

    Deploy error fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import WhatWeDo from "./components/WhatWeDo/WhatWeDo";
 import Resume from "./components/Skills/Resume"
 import Portfolio from "./components/Portfolio/Portfolio";
 import CTO from "./components/CTO/CTO";
-import FAQ from "./components/FAQ/FAQ";
-import Carousel from "./components/ClientSpeak/Carousel";
+/* import FAQ from "./components/FAQ/FAQ";
+import Carousel from "./components/ClientSpeak/Carousel"; */
 import ContactUs from "./components/ContactUs/ContactUs";
 import Footer from "./components/Footer/Footer";
 
